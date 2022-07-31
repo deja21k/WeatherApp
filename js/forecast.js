8b6d67142f771459290842f97330a29e91b65157
deja21k <deja21k@icloud.com>
2022-07-31 14:37:29 +0200

    created functions for Get Weather and get DDDDDcity'

---
+++ js/forecast.js
@@ -0,0 +1,26 @@
+const key = 'waOJE6XSkoUhR4nvG8FBaV5eTpGSwGNN';
+
+// ~~~~ Get Weather Information ~~~~
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// ~~~~ Get City Information ~~~~
+
+const getCity =  async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
